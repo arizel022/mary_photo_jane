21a12aef75d1cc6c9026010bba389e1a7349a280
Alex Mykhailov <arizel022@gmail.com>
2022-09-13 21:32:36 +0300

    add desktop

---
+++ docs/js/main.js
@@ -0,0 +1,70 @@
+$(function() {
+    $('.menu__list').hide();
+    //-----------------------------------------работа кнопки меню
+    $('.menu__btn').click(function () {
+        $(this).toggleClass('active');
+        $('.menu__list').slideToggle();
+	});
+
+    //---------------------slick
+    $('.promo').slick({
+		dots: true,
+        infinite: true,
+        vertical: true,
+        arrows: false,
+        appendDots: $('.dots'),
+		autoplay: true
+	});
+    //-----------------------------------------работа eye
+    $('.view-btn').click(function () {
+        $(this).toggleClass('active');
+        $('.menu__btn').toggleClass('off');
+        $('.header__logo').toggleClass('off');
+        $('.dots').toggleClass('off');
+        $('.promo__item').toggleClass('off');
+	});
+	
+	var containerEl1 = document.querySelector('[data-ref="container"]');
+	var containerEl2 = document.querySelector('[data-ref="container2"]');
+   
+	var config = {
+	  controls: {
+		scope: 'local'
+	  }
+	};
+   
+	var mixer1 = mixitup(containerEl1, config);
+	var mixer2 = mixitup(containerEl2, config);
+
+});
+
+
+
+
+
+$(document).ready(function() {
+	$('.gallery__list').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function(item) {
+				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank"></a>';
+			}
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300,
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
+});
