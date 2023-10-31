f4e388834c9aaa0d460310fa64af952ddfb83c1d
Alex Mykhailov <arizel022@gmail.com>
2023-10-31 19:44:58 -0400

    added new structure of repo

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,48 +1,68 @@
-$(function() {
-    $('.menu__list').hide();
-    //-----------------------------------------работа кнопки меню
-    $('.menu__btn').click(function () {
-        $(this).toggleClass('active');
-        $('.menu__list').slideToggle();
+
+
+$(function () {
+	$('.menu__list').hide();
+	//-----------------------------------------работа кнопки меню
+	$('.menu-btn').click(function () {
+		$(this).toggleClass('active');
+		$('.header').toggleClass('show');
 	});
 
-    //---------------------slick
-    $('.promo').slick({
+	//---------------------slick
+	$('.promo__content').slick({
 		dots: true,
-        infinite: true,
-        vertical: true,
-        arrows: false,
-        appendDots: $('.dots'),
-		autoplay: true
+		infinite: true,
+		// vertical: true,
+		arrows: false,
+		appendDots: $('.dots'),
+		swipeToSlide: true
+		// autoplay: true
+		// responsive: [
+		// 	{
+		// 		breakpoint: 900,
+		// 		settings: {
+		// 			vertical: false,
+		// 			fade: true,
+		// 			autoplay: true
+		// 		}
+		// 	}
+		// ]
 	});
-    //-----------------------------------------работа eye
-    $('.view-btn').click(function () {
-        $(this).toggleClass('active');
-        $('.menu__btn').toggleClass('off');
-        $('.header__logo').toggleClass('off');
-        $('.dots').toggleClass('off');
-        $('.promo__item').toggleClass('off');
+	//-----------------------------------------работа eye
+	$('.promo__view-btn').click(function () {
+		$(this).toggleClass('active');
+		$('.menu__btn').toggleClass('off');
+		$('.hero__logo-img').toggleClass('off');
+		$('.dots').toggleClass('off');
+		$('.promo__item').toggleClass('off');
 	});
+	//-----------------------------------------dropdown in nav
+	$('.header__link--dots').click(function () {
+		$(this).toggleClass('active');
+		$('.header__dropdown-list').toggleClass('show');
+	});
+
+
+
 	
+
+
+
 	var containerEl1 = document.querySelector('[data-ref="container"]');
 	var containerEl2 = document.querySelector('[data-ref="container2"]');
-   
 	var config = {
-	  controls: {
-		scope: 'local'
-	  }
+		controls: {
+			scope: 'local'
+		}
 	};
-   
+
 	var mixer1 = mixitup(containerEl1, config);
 	var mixer2 = mixitup(containerEl2, config);
 
 });
 
 
-
-
-
-$(document).ready(function() {
+$(document).ready(function () {
 	$('.gallery__list').magnificPopup({
 		delegate: 'a',
 		type: 'image',
@@ -51,8 +71,8 @@ $(document).ready(function() {
 		mainClass: 'mfp-with-zoom mfp-img-mobile',
 		image: {
 			verticalFit: true,
-			titleSrc: function(item) {
-				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank"></a>';
+			titleSrc: function (item) {
+				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="' + item.el.attr('data-source') + '" target="_blank"></a>';
 			}
 		},
 		gallery: {
@@ -61,10 +81,37 @@ $(document).ready(function() {
 		zoom: {
 			enabled: true,
 			duration: 300,
-			opener: function(element) {
+			opener: function (element) {
 				return element.find('img');
 			}
 		}
-		
+
+	});
+});
+
+$(document).ready(function () {
+	$('.collection__list').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function (item) {
+				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="' + item.el.attr('data-source') + '" target="_blank"></a>';
+			}
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300,
+			opener: function (element) {
+				return element.find('img');
+			}
+		}
+
 	});
 });
